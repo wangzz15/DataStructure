64bb7d1436723bd524b65eea9502b2f5cb86f7c6
wangzz15 <wangzz15@tsinghua.org.cn>
2020-09-04 17:43:42 +0800

    change name

--- FenwichTree.py
+++ FenwichTree.py
@@ -1,28 +1,28 @@
 class FenwichTree:
     def __init__(self, nums):
-        self.BIT_arr = [0] * (len(nums) + 1)
+        self.arr = [0] * (len(nums) + 1)
         for i in range(len(nums)):
-            self.BIT_arr[i + 1] = nums[i]
-        for i in range(1, len(self.BIT_arr)):
+            self.arr[i + 1] = nums[i]
+        for i in range(1, len(self.arr)):
             j = i + (i & -i)
-            if j < len(self.BIT_arr):
-                self.BIT_arr[j] += self.BIT_arr[i]
+            if j < len(self.arr):
+                self.arr[j] += self.arr[i]
 
     def update(self, i, delta):
         i += 1
-        while i < len(self.BIT_arr):
-            self.BIT_arr[i] += delta
+        while i < len(self.arr):
+            self.arr[i] += delta
             i += (i & (-i))
             
     def setVal(self, i, val):
         i += 1
-        self.update(i, val - self.BIT_arr[i])
+        self.update(i, val - self.arr[i])
 
     def prefix(self, i):
         i += 1
         res = 0
         while i > 0:
-            res += self.BIT_arr[i]
+            res += self.arr[i]
             i -= (i & (-i))
         return res
 
